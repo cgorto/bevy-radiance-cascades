a1fdb913c41ed2c5fb527f226787643475642d81
cgorto <gorto005@gmail.com>
2025-04-18 12:20:47 -0600

    before i transition to mesh and custom material

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::color::{color_difference::EuclideanDistance, palettes::css};
 use bevy::prelude::*;
+use bevy::render::render_resource::TextureUsages;
 use bevy::render::{
     render_asset::RenderAssetUsages,
     render_resource::{Extent3d, TextureDimension, TextureFormat},
@@ -17,24 +18,30 @@ struct CanvasImage(Handle<Image>);
 
 fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>, window: Query<&Window>) {
     commands.spawn(Camera2d);
-
+    println!("we reach here!");
     if let Ok(window) = window.get_single() {
+        println!("we've got the window");
         let size = Extent3d {
             width: window.resolution.width().round() as u32,
             height: window.resolution.height().round() as u32,
             depth_or_array_layers: 1,
         };
-        let image = Image::new_fill(
+        println!("we've got the size");
+        let mut image = Image::new_fill(
             size,
             TextureDimension::D2,
-            &(css::BEIGE.to_u8_array()),
+            &[255, 255, 255, 255],
             TextureFormat::Rgba8UnormSrgb,
             RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
         );
-
+        image.texture_descriptor.usage = TextureUsages::COPY_DST
+            | TextureUsages::STORAGE_BINDING
+            | TextureUsages::TEXTURE_BINDING;
+        println!("created image");
         let handle = images.add(image);
 
         commands.spawn(Sprite::from_image(handle.clone()));
         commands.insert_resource(CanvasImage(handle));
+        println!("sys done");
     }
 }
