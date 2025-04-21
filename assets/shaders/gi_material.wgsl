b82ae39384e2f46bd945cc0cfcebe9f1d2f524a5
cgorto <christian.gorton@colorado.edu>
2025-04-21 11:28:03 -0600

    Shader is dispatching

--- assets/shaders/gi_material.wgsl
+++ assets/shaders/gi_material.wgsl
@@ -1,12 +1,59 @@
-#import bevy_sprite::mesh2d_vertex_output::VertexOutput
-// we can import items from shader modules in the assets folder with a quoted path
-#import "shaders/custom_material_import.wgsl"::COLOR_MULTIPLIER
 
-@group(0) @binding(0) var<uniform> material_color: vec4<f32>;
-@group(0) @binding(1) var base_color_texture: texture_2d<f32>;
-@group(0) @binding(2) var base_color_sampler: sampler;
+// The shader reads the previous frame's state from the `input` texture, and writes the new state of
+// each pixel to the `output` texture. The textures are flipped each step to progress the
+// simulation.
+// Two textures are needed for the game of life as each pixel of step N depends on the state of its
+// neighbors at step N-1.
 
-@fragment
-fn fragment(mesh: VertexOutput) -> @location(0) vec4<f32> {
-    return material_color * textureSample(base_color_texture, base_color_sampler, mesh.uv) * COLOR_MULTIPLIER;
+@group(0) @binding(0) var input: texture_storage_2d<rgba8unorm, read>;
+
+@group(0) @binding(1) var output: texture_storage_2d<rgba8unorm, write>;
+
+
+
+@compute @workgroup_size(8, 8, 1)
+fn init(@builtin(global_invocation_id) invocation_id: vec3<u32>, @builtin(num_workgroups) num_workgroups: vec3<u32>) {
+    let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
+
+    let randomNumber = randomFloat(invocation_id.y << 16u | invocation_id.x);
+    let alive = randomNumber > 0.9;
+    let color = vec4<f32>(f32(alive));
+
+    textureStore(output, location, color);
+}
+
+fn is_alive(location: vec2<i32>, offset_x: i32, offset_y: i32) -> i32 {
+    let value: vec4<f32> = textureLoad(input, location + vec2<i32>(offset_x, offset_y));
+    return i32(value.x);
+}
+
+fn count_alive(location: vec2<i32>) -> i32 {
+    return is_alive(location, -1, -1) +
+           is_alive(location, -1,  0) +
+           is_alive(location, -1,  1) +
+           is_alive(location,  0, -1) +
+           is_alive(location,  0,  1) +
+           is_alive(location,  1, -1) +
+           is_alive(location,  1,  0) +
+           is_alive(location,  1,  1);
+}
+
+@compute @workgroup_size(8, 8, 1)
+fn update(@builtin(global_invocation_id) invocation_id: vec3<u32>) {
+    let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
+
+    let n_alive = count_alive(location);
+
+    var alive: bool;
+    if (n_alive == 3) {
+        alive = true;
+    } else if (n_alive == 2) {
+        let currently_alive = is_alive(location, 0, 0);
+        alive = bool(currently_alive);
+    } else {
+        alive = false;
+    }
+    let color = vec4<f32>(f32(alive));
+
+    textureStore(output, location, color);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use bevy::{
-    asset::RenderAssetUsages,
+    pbr::prepare_prepass_view_bind_group,
     prelude::*,
-    reflect::TypePath,
-    render::{extract_resource::ExtractResource, render_resource::*},
-    sprite::{Material2d, Material2dPlugin},
+    render::{
+        Render, RenderApp, RenderSet,
+        extract_resource::{ExtractResource, ExtractResourcePlugin},
+        graph,
+        render_asset::{RenderAssetUsages, RenderAssets},
+        render_graph::{self, RenderGraph, RenderLabel},
+        render_resource::{binding_types::texture_storage_2d, *},
+        renderer::{RenderContext, RenderDevice},
+        texture::GpuImage,
+    },
 };
+
+use std::borrow::Cow;
 
 const SHADER_ASSET_PATH: &str = "shaders/gi_material.wgsl";
 
+const SIZE: (u32, u32) = (1280, 720);
+const WORKGROUP_SIZE: u32 = 8;
+
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins))
+        .add_plugins((DefaultPlugins, CascadePlugin))
         .add_systems(Startup, setup)
+        .add_systems(Update, switch_textures)
         .run();
 }
 
@@ -53,5 +66,204 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>, window: Quer
             texture_a: image0,
             texture_b: image1,
         });
+    }
+}
+
+fn switch_textures(images: Res<LightingImages>, mut sprite: Single<&mut Sprite>) {
+    if sprite.image == images.texture_a {
+        sprite.image = images.texture_b.clone_weak();
+    } else {
+        sprite.image = images.texture_a.clone_weak();
+    }
+}
+
+struct CascadePlugin;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, RenderLabel)]
+struct CascadeLabel;
+
+impl Plugin for CascadePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(ExtractResourcePlugin::<LightingImages>::default());
+        let render_app = app.sub_app_mut(RenderApp);
+        render_app.add_systems(
+            Render,
+            prepare_bind_group.in_set(RenderSet::PrepareBindGroups),
+        );
+
+        let mut render_graph = render_app.world_mut().resource_mut::<RenderGraph>();
+        render_graph.add_node(CascadeLabel, CascadeNode::default());
+        render_graph.add_node_edge(CascadeLabel, bevy::render::graph::CameraDriverLabel);
+    }
+
+    fn finish(&self, app: &mut App) {
+        let render_app = app.sub_app_mut(RenderApp);
+        render_app.init_resource::<CascadePipeline>();
+    }
+}
+
+#[derive(Resource)]
+struct LightingImageBindGroups([BindGroup; 2]);
+
+fn prepare_bind_group(
+    mut commands: Commands,
+    pipeline: Res<CascadePipeline>,
+    gpu_images: Res<RenderAssets<GpuImage>>,
+    lighting_images: Res<LightingImages>,
+    render_device: Res<RenderDevice>,
+) {
+    let view_a = gpu_images.get(&lighting_images.texture_a).unwrap();
+    let view_b = gpu_images.get(&lighting_images.texture_b).unwrap();
+    let bind_group_0 = render_device.create_bind_group(
+        None,
+        &pipeline.texture_bind_group_layout,
+        &BindGroupEntries::sequential((&view_a.texture_view, &view_b.texture_view)),
+    );
+
+    let bind_group_1 = render_device.create_bind_group(
+        None,
+        &pipeline.texture_bind_group_layout,
+        &BindGroupEntries::sequential((&view_a.texture_view, &view_b.texture_view)),
+    );
+
+    commands.insert_resource(LightingImageBindGroups([bind_group_0, bind_group_1]));
+}
+
+#[derive(Resource)]
+struct CascadePipeline {
+    texture_bind_group_layout: BindGroupLayout,
+    init_pipeline: CachedComputePipelineId,
+    update_pipeline: CachedComputePipelineId,
+}
+
+impl FromWorld for CascadePipeline {
+    fn from_world(world: &mut World) -> Self {
+        let render_device = world.resource::<RenderDevice>();
+        let texture_bind_group_layout = render_device.create_bind_group_layout(
+            "LightingImages",
+            &BindGroupLayoutEntries::sequential(
+                ShaderStages::COMPUTE,
+                (
+                    texture_storage_2d(TextureFormat::Rgba8Unorm, StorageTextureAccess::ReadOnly),
+                    texture_storage_2d(TextureFormat::Rgba8Unorm, StorageTextureAccess::WriteOnly),
+                ),
+            ),
+        );
+        let shader = world.load_asset(SHADER_ASSET_PATH);
+        let pipeline_cache = world.resource::<PipelineCache>();
+        let init_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
+            label: None,
+            layout: vec![texture_bind_group_layout.clone()],
+            push_constant_ranges: Vec::new(),
+            shader: shader.clone(),
+            shader_defs: vec![],
+            entry_point: Cow::from("init"),
+            zero_initialize_workgroup_memory: false,
+        });
+
+        let update_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
+            label: None,
+            layout: vec![texture_bind_group_layout.clone()],
+            push_constant_ranges: Vec::new(),
+            shader,
+            shader_defs: vec![],
+            entry_point: Cow::from("update"),
+            zero_initialize_workgroup_memory: false,
+        });
+
+        CascadePipeline {
+            texture_bind_group_layout,
+            init_pipeline,
+            update_pipeline,
+        }
+    }
+}
+
+enum CascadeState {
+    Loading,
+    Init,
+    Update(usize),
+}
+
+struct CascadeNode {
+    state: CascadeState,
+}
+
+impl Default for CascadeNode {
+    fn default() -> Self {
+        Self {
+            state: CascadeState::Loading,
+        }
+    }
+}
+
+impl render_graph::Node for CascadeNode {
+    fn update(&mut self, world: &mut World) {
+        let pipeline = world.resource::<CascadePipeline>();
+        let pipeline_cache = world.resource::<PipelineCache>();
+
+        match self.state {
+            CascadeState::Loading => {
+                match pipeline_cache.get_compute_pipeline_state(pipeline.init_pipeline) {
+                    CachedPipelineState::Ok(_) => {
+                        self.state = CascadeState::Init;
+                    }
+                    CachedPipelineState::Err(_) => {
+                        panic!("the pipeline is fucked")
+                    }
+                    _ => {}
+                }
+            }
+            CascadeState::Init => {
+                if let CachedPipelineState::Ok(_) =
+                    pipeline_cache.get_compute_pipeline_state(pipeline.update_pipeline)
+                {
+                    self.state = CascadeState::Update(1);
+                }
+            }
+            CascadeState::Update(0) => {
+                self.state = CascadeState::Update(1);
+            }
+            CascadeState::Update(1) => {
+                self.state = CascadeState::Update(0);
+            }
+            CascadeState::Update(_) => unreachable!(),
+        }
+    }
+
+    fn run(
+        &self,
+        _graph: &mut render_graph::RenderGraphContext,
+        render_context: &mut RenderContext,
+        world: &World,
+    ) -> Result<(), render_graph::NodeRunError> {
+        let bind_groups = &world.resource::<LightingImageBindGroups>().0;
+        let pipeline_cache = world.resource::<PipelineCache>();
+        let pipeline = world.resource::<CascadePipeline>();
+
+        let mut pass = render_context
+            .command_encoder()
+            .begin_compute_pass(&ComputePassDescriptor::default());
+
+        match self.state {
+            CascadeState::Loading => {}
+            CascadeState::Init => {
+                let init_pipeline = pipeline_cache
+                    .get_compute_pipeline(pipeline.init_pipeline)
+                    .unwrap();
+                pass.set_bind_group(0, &bind_groups[0], &[]);
+                pass.set_pipeline(init_pipeline);
+                pass.dispatch_workgroups(SIZE.0 / WORKGROUP_SIZE, SIZE.1 / WORKGROUP_SIZE, 1);
+            }
+            CascadeState::Update(index) => {
+                let update_pipeline = pipeline_cache
+                    .get_compute_pipeline(pipeline.update_pipeline)
+                    .unwrap();
+                pass.set_bind_group(0, &bind_groups[index], &[]);
+                pass.set_pipeline(update_pipeline);
+                pass.dispatch_workgroups(SIZE.0 / WORKGROUP_SIZE, SIZE.1 / WORKGROUP_SIZE, 1);
+            }
+        }
+        Ok(())
     }
 }
